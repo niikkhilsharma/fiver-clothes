b5c38cef4c7c5a371e4a120a70d671baf26995a3
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2025-02-14 22:12:35 +0530

    adjusted screen

--- components/header.tsx
+++ components/header.tsx
@@ -42,7 +42,7 @@ export default function Header({
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-[62%] py-2 sm:top-[1.7rem] sm:h-[initial] sm:-translate-x-1/2 sm:py-0">
+      <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
